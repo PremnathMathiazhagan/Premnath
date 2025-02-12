b489653bc0e11c59c1755ddaa7eaa0cbb3fc965b
Prem nath <premcs444@gmail.com>
2025-02-12 16:52:02 +0530

    day18bacchanges

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main()
+    {
+        PrintName("Prem",10);
+ 
+   }
+    static void PrintName(string name, int iterations)
+    {
+        for (int iteration=1; iteration<=iterations; iteration++)
+        {
+            Console.WriteLine($"Hello {name.ToUpper()}! {iterations}");
+        }
+    }
+}
